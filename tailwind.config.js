b46921018a84e47de4c7a4b999b7801a651dd926
Zidan <technicalzidan86@gmail.com>
2024-10-28 21:01:21 +0600

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
-    },
+    },  
+    colors: {
+        'white-transparent': '#FFFFFF00',
+      },
     plugins: [],
   }
